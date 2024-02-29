c50ac1720f3d79bf7cfa559a4237743baf57173d
김명찬 <myeongchan@gimmyeongchan-ui-MacBookPro.local>
2024-02-29 14:16:14 +0900

    chore : tailwindcss 애니메이션 커스텀 예제 코드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: "fadeIn 2s ease-in-out",
+      },
+
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
